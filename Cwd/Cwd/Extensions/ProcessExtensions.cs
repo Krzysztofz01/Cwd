4208e346a25161d89ce5e3bac06401ffad46521b
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-07-13 14:29:12 +0200

    Process end time fix

--- Cwd/Cwd/Extensions/ProcessExtensions.cs
+++ Cwd/Cwd/Extensions/ProcessExtensions.cs
@@ -4,12 +4,14 @@ namespace Cwd.Extensions
 {
     public static class ProcessExtensions
     {
-        // Workaround for https://github.com/dotnet/runtime/issues/27128
-        // Solution found in https://github.com/CopyText/TextCopy
+        // Issue workaround
+        // https://github.com/dotnet/runtime/issues/27128
         public static bool DoubleWaitForExit(this Process process)
         {
-            var exit = process.WaitForExit(500);
-            if (exit) process.WaitForExit();
+            const int waitTimeMs = 250;
+            
+            var exit = process.WaitForExit(waitTimeMs * 2);
+            if (exit) process.WaitForExit(waitTimeMs);
 
             return exit;
         }
