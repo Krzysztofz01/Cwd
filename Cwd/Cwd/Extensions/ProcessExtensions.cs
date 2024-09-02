99f82e6423906fa8be6723caee3670815480014f
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2024-09-02 15:18:25 +0200

    DLLImport replaced with LibraryImport and various code cleanups

--- Cwd/Cwd/Extensions/ProcessExtensions.cs
+++ Cwd/Cwd/Extensions/ProcessExtensions.cs
@@ -1,19 +1,18 @@
 ﻿using System.Diagnostics;
 
-namespace Cwd.Extensions
-{
-    public static class ProcessExtensions
-    {
-        // Issue workaround
-        // https://github.com/dotnet/runtime/issues/27128
-        public static bool DoubleWaitForExit(this Process process)
-        {
-            const int waitTimeMs = 250;
-            
-            var exit = process.WaitForExit(waitTimeMs * 2);
-            if (exit) process.WaitForExit(waitTimeMs);
+namespace Cwd.Extensions;
 
-            return exit;
-        }
+public static class ProcessExtensions
+{
+    // Issue workaround
+    // https://github.com/dotnet/runtime/issues/27128
+    public static bool DoubleWaitForExit(this Process process)
+    {
+        const int waitTimeMs = 250;
+        
+        var exit = process.WaitForExit(waitTimeMs * 2);
+        if (exit) process.WaitForExit(waitTimeMs);
+
+        return exit;
     }
 }
--- Cwd/Cwd/Program.cs
+++ Cwd/Cwd/Program.cs
@@ -4,67 +4,66 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
-namespace Cwd
+namespace Cwd;
+
+class Program
 {
-    class Program
+    private const int _success = 0;
+    private const int _failure = 1;
+
+    static int Main(string[] args)
     {
-        private const int _success = 0;
-        private const int _failure = 1;
-
-        static int Main(string[] args)
+        try
         {
-            try
+            if (args.HasParam("-h", "--help"))
             {
-                if (args.HasParam("-h", "--help"))
-                {
-                    Help.Print();
-
-                    return _success;
-                }
-
-                var printCurrentDirectory = args.HasParam("-p", "--print");
-                var addAdditionalEnterForJump = args.HasParam("-j", "--jump");
-
-                var clipboardService = GetClipboardService();
-
-                string currentDirectory = Directory.GetCurrentDirectory();
-
-                string valueToCopy = addAdditionalEnterForJump
-                    ? $"cd {currentDirectory}{Environment.NewLine}"
-                    : currentDirectory;
-
-                clipboardService.CopyToClipboard(valueToCopy);
-
-                if (printCurrentDirectory) Console.WriteLine($"{Environment.NewLine}{currentDirectory}{Environment.NewLine}");
-
-                Console.WriteLine("Current directory path copied to clipboard...");
+                Help.Print();
 
                 return _success;
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return _failure;
-            }
-        }
 
-        private static IClipboardService GetClipboardService()
+            var printCurrentDirectory = args.HasParam("-p", "--print");
+            var addAdditionalEnterForJump = args.HasParam("-j", "--jump");
+
+            var clipboardService = GetClipboardService();
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            string valueToCopy = addAdditionalEnterForJump
+                ? $"cd {currentDirectory}{Environment.NewLine}"
+                : currentDirectory;
+
+            clipboardService.CopyToClipboard(valueToCopy);
+
+            if (printCurrentDirectory) Console.WriteLine($"{Environment.NewLine}{currentDirectory}{Environment.NewLine}");
+
+            Console.WriteLine("Current directory path copied to clipboard...");
+
+            return _success;
+        }
+        catch(Exception e)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return new WindowsClipboardService();
-            }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                return new LinuxClipboardService();
-            }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-            }
-
-            throw new PlatformNotSupportedException("This operation system is not supported.");
+            Console.WriteLine(e.Message);
+            return _failure;
         }
+    }
+
+    private static IClipboardService GetClipboardService()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return new WindowsClipboardService();
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return new LinuxClipboardService();
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+        }
+
+        throw new PlatformNotSupportedException("This operation system is not supported.");
     }
 }
